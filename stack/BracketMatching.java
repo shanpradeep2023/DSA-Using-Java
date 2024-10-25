8eb66ffd7b0f2f1a0783bb3294373c3d0f37bd05
Pradeep <shanpradeep2023@gmail.com>
2024-10-25 17:00:22 +0530

    Did bracket matching problem

---
+++ stack/BracketMatching.java
@@ -0,0 +1,44 @@
+package stack;
+
+import java.util.Stack;
+
+public class BracketMatching {
+    public static boolean cheak(String s){
+        Stack<Character> stack = new Stack<>();
+        for(char a : s.toCharArray()){
+            switch (a) {
+                case '{':
+                case '[':
+                case '(':
+                    stack.push(a);
+                    break;
+
+                case ')':
+                    if(stack.isEmpty() || stack.pop() != '(')
+                        return false;
+                    break;
+                
+                case '}':
+                    if(stack.isEmpty() || stack.pop() != '{')
+                        return false;
+                    break;
+
+                case ']':
+                    if(stack.isEmpty() || stack.pop() != '[')
+                        return false;
+                    break;
+            
+                default:
+                    break;
+            }
+
+            
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s1 = "{{[[(())]]}}";
+        System.out.println(cheak(s1));
+    }
+}
